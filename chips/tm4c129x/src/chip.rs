52d26da23f385338e9bb8ff1fcae5d436f878ae3
Unknown <alexander.muellner@outlook.com>
2018-01-22 01:25:49 +0100

    EK-TM4C1294XL Support
    Update to the latest TOCK-OS Version

---
+++ chips/tm4c129x/src/chip.rs
@@ -0,0 +1,71 @@
+//! Interrupt mapping and DMA channel setup.
+
+use cortexm4;
+use gpio;
+use kernel::Chip;
+use kernel::common::{RingBuffer, Queue};
+use helpers::{DeferredCall, Task};
+use nvic;
+use gpt;
+use uart;
+
+
+pub struct Tm4c129x {
+    pub mpu: cortexm4::mpu::MPU,
+    pub systick: cortexm4::systick::SysTick,
+}
+
+impl Tm4c129x {
+    pub unsafe fn new() -> Tm4c129x {
+      
+        Tm4c129x {
+            mpu: cortexm4::mpu::MPU::new(),
+            systick: cortexm4::systick::SysTick::new(),
+        }
+    }
+}
+
+impl Chip for Tm4c129x {
+    type MPU = cortexm4::mpu::MPU;
+    type SysTick = cortexm4::systick::SysTick;
+
+    fn service_pending_interrupts(&mut self) {
+        use nvic::*;
+		
+		unsafe{
+			loop {
+				if let Some(task) = DeferredCall::next_pending() {
+                   
+				} else if let Some(interrupt) = cortexm4::nvic::next_pending() {
+		            match interrupt {
+		                     
+		                    UART7 => uart::UART7.handle_interrupt(),
+		                    TIMER0A => gpt::TIMER0.handle_interrupt(),
+		                    _ => {
+				                   panic!("unhandled interrupt {}", interrupt); 
+		                    }
+		                }
+			            let n = cortexm4::nvic::Nvic::new(interrupt);
+		                n.clear_pending();
+		                n.enable();
+		                
+		            } else {
+						break;            	
+			        }
+				 }
+		}
+    }
+    
+    fn has_pending_interrupts(&self) -> bool {
+        unsafe { cortexm4::nvic::has_pending() || DeferredCall::has_tasks() }
+    }
+
+    fn mpu(&self) -> &cortexm4::mpu::MPU {
+        &self.mpu
+    }
+
+    fn systick(&self) -> &cortexm4::systick::SysTick {
+        &self.systick
+    }
+    
+}
